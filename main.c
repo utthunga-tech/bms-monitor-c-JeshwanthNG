09aa48803521548b95332178a835f319bb936e83
jeswanthng <jeswanthng@gmail.com>
2021-10-04 09:56:25 +0530

    finxed warning in main file, removed unwanted file checker.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) 
+int main() 
 {
 	
 	//TODO : Delete below commented functions later
